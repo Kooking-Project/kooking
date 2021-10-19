797bdbb099f987ae27e3918c9dc0cbaab96b1cec
wjjeon91 <68631971+wjjeon91@users.noreply.github.com>
2021-10-19 15:56:28 +0900

    pull 전 커밋

--- src/main/java/com/kooking/service/AdminService.java
+++ src/main/java/com/kooking/service/AdminService.java
@@ -5,11 +5,23 @@ import java.util.List;
 import com.kooking.dto.UserDTO;
 
 public interface AdminService {
+	/**
+	 * 회원 상태 변경
+	 * */
 	void changeUserStatus(int adminStatus, UserDTO user) throws Exception;
 	
+	/**
+	 * 댓글 삭제
+	 * */
 	void commentDelete(int adminNo, int commentNo) throws Exception;
 
+	/**
+	 * 게시글 삭제
+	 * */
 	void postDelete(int adminNo, int postNo) throws Exception;
 	
+	/**
+	 * 유저정보 전체 검색
+	 * */
 	List<UserDTO> userSelectAll(int adminNo) throws Exception;
 }
