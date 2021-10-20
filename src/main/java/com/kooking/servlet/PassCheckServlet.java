7f698432760a624f2d786a9167ca906443e77dfa
ahnjjun <88135979+ahnjjun@users.noreply.github.com>
2021-10-20 10:54:25 +0900

    passcheck
    dd

--- src/main/java/com/kooking/servlet/PassCheckServlet.java
+++ src/main/java/com/kooking/servlet/PassCheckServlet.java
@@ -26,7 +26,7 @@ public class PassCheckServlet extends HttpServlet {
 
 		PrintWriter out = response.getWriter();
 
-		if (pwd == pwdConfirm) {
+		if (pwd.equals( pwdConfirm )) {
 			out.print("암호가 일치합니다.");
 		} else {
 			out.print("암호가 일치하지 않습니다.");
